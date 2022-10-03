a5fa7e773fb6f2579cb1014c85d13fc80a10316c
limdongju <ghfflr456@gmail.com>
2022-10-03 16:18:33 +0900

    ### 입력값 결과 검증 로직 구현

--- src/main/java/baseball/controller/InputCodeController.java
+++ src/main/java/baseball/controller/InputCodeController.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class InputCodeController {
     public void inputCode(String data, List<Integer> numbers){
-        if(expInputData(data)){
-            System.out.println("### 검증 성공");
-            new InputCodeModel().dataInspection(data,numbers);
-        }
+        expLengthInputData(data);
+        expNumberInputData(data);
+        new InputCodeModel().dataInspection(data,numbers);
     }
-    private Boolean expInputData(String data){
+    private void expLengthInputData(String data){
         if (data.length() != 3){
             throw new IllegalArgumentException("문자열 입력 길이 오류");
         }
-        if (!data.matches("[0-9]+")){
+    }
+    private void expNumberInputData(String data){
+        if(!data.matches("[0-9]+")){
             throw new IllegalArgumentException("숫자가 아닌 값 입력");
         }
-        return true;
     }
 }
 
--- src/main/java/baseball/model/InputCodeModel.java
+++ src/main/java/baseball/model/InputCodeModel.java
@@ -4,10 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputCodeModel {
-
     public void dataInspection(String data, List<Integer> numbers){
         List<String> datas = Arrays.asList(data.split(""));
         int strike = 0;
         int ball = 0;
+        for (int i=0;i<3;i++){
+            Integer nowData = Integer.valueOf(datas.get(i));
+            strike = strikeCounting(nowData,numbers.get(i),strike);
+            ball = ballCounting(nowData,numbers,ball,i);
+        }
+    }
+    private int strikeCounting(Integer data, Integer number, int strike){
+        if(data == number){
+            strike += 1;
+        }
+        return strike;
+    }
+
+    private int ballCounting(Integer data, List<Integer> numbers, int ball, int step){
+        if(numbers.indexOf(data) != step && numbers.indexOf(data) > -1){
+            ball += 1;
+        }
+        return ball;
     }
 }
